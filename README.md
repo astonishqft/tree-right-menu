22d618eff241d27ed0a7d1ce66d49368afe55614
qi.futao <qi.futao@iwhalecloud.com>
2022-07-26 09:11:16 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# tree-right-menu
-antd4 + Tree 实现右键菜单
+# umi project
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ yarn
+```
+
+Start the dev server,
+
+```bash
+$ yarn start
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,114 @@
+import { Tree, Dropdown, Menu } from 'antd';
+import type { DataNode, TreeProps } from 'antd/es/tree';
+
+const treeData: DataNode[] = [
+  {
+    title: '中国',
+    key: '0-0',
+    children: [
+      {
+        title: '江苏',
+        key: '0-0-0',
+        children: [
+          {
+            title: '南京',
+            key: '0-0-0-0',
+          },
+          {
+            title: '扬州',
+            key: '0-0-0-1',
+          },
+        ],
+      },
+      {
+        title: '上海',
+        key: '0-0-1',
+        children: [
+          {
+            title: '长宁区',
+            key: '0-0-1-0',
+          },
+          {
+            title: '徐汇区',
+            key: '0-0-2-1',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default function IndexPage() {
+  const onSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
+    console.log('selected', selectedKeys, info);
+  };
+
+  const onCheck: TreeProps['onCheck'] = (checkedKeys, info) => {
+    console.log('onCheck', checkedKeys, info);
+  };
+
+  const menu = (
+    <Menu
+      items={[
+        {
+          key: '1',
+          label: (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.antgroup.com"
+            >
+              1st menu item
+            </a>
+          ),
+        },
+        {
+          key: '2',
+          label: (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.aliyun.com"
+            >
+              2nd menu item
+            </a>
+          ),
+        },
+        {
+          key: '3',
+          label: (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.luohanacademy.com"
+            >
+              3rd menu item
+            </a>
+          ),
+        },
+      ]}
+    />
+  );
+
+  const titleRender = (nodeData: DataNode) => {
+    return (
+      <Dropdown overlay={menu} trigger={['contextMenu']}>
+        <div>{nodeData.title}</div>
+      </Dropdown>
+    );
+  };
+
+  return (
+    <div>
+      <Tree
+        defaultExpandedKeys={['0-0-0', '0-0-1']}
+        // defaultSelectedKeys={['0-0-0', '0-0-1']}
+        // defaultCheckedKeys={['0-0-0', '0-0-1']}
+        titleRender={titleRender}
+        onSelect={onSelect}
+        onCheck={onCheck}
+        treeData={treeData}
+      />
+    </div>
+  );
+}
